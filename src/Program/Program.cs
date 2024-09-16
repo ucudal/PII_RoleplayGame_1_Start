554b44b8575984b90ffa34088b80194c43aefe82
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2024-09-15 22:45:55 -0300

    Personaje e items de Ramiro Lopez

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Personaje e items de Ramiro Lopez
+Item espadaDeFuego = new Item("Espada de Fuego", 50, 0);
+Item cascoMagico = new Item("Casco Mágico", 0, 30);
+List<Item> dondarItems = new List<Item> { espadaDeFuego, cascoMagico };
+Dwarf dondarRami = new Dwarf("Dondar", 100, 40, dondarItems);
+Console.WriteLine(dondarRami.GetTotalAttackValue());
