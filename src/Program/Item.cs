d6fa11461847af2750270cb4b8dd2e9d7dbe3866
Dolphin <ignacioalonsosclavo@gmail.com>
2025-09-21 22:02:44 -0300

    Se_agrego_Clase_Item
    Se agrego la clase Item

---
+++ src/Program/Item.cs
@@ -0,0 +1,10 @@
+namespace Program;
+
+public class Item
+{
+    public string Name { get; set; }
+    public int MagicDamage { get; set; }
+    public int Damage { get; set; }
+    public int Armor { get; set; }
+    
+}
