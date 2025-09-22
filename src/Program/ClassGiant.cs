e2241ad0b346d2c433467ecb5466a75bda6db332
FrancoRevello18 <francorevello18@gmail.com>
2025-09-22 01:06:38 -0300

    ClassGiant
    Se agregó tremendo personaje

---
+++ src/Program/ClassGiant.cs
@@ -0,0 +1,70 @@
+using System.IO.Pipes;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+namespace Program.CharacterClasses;
+
+public class Giant : ICharacter
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int MeleeDamage { get; set; }
+    public int TotalHealth {get; set;}
+    public Inventory Inventory {get; set;}
+    public int Armor
+    {
+        get
+        {
+            int totalArmor = 0;
+            foreach (Items item in this.Inventory.ListInventory)
+            {
+                totalArmor += item.Armor;
+            }
+
+            return totalArmor;
+        }
+    }
+    
+    public Giant(string name, int health, int meleeDamage, Inventory listInventory)
+    {
+        this.Name = name;
+        this.Health = health;
+        this.MeleeDamage = meleeDamage;
+        this.TotalHealth = health;
+        this.Inventory = listInventory;
+        
+       
+    }
+
+    public void Attack(ICharacter target)
+    {
+        int additionalDamage = 0;
+        foreach(Items item in this.Inventory.ListInventory)
+        {
+            additionalDamage += item.Damage;
+        }
+        target.Health -= ((MeleeDamage + additionalDamage) - target.Armor);
+    }
+    
+
+    public void Heal()
+    {
+        this.Health = TotalHealth;
+    }
+
+    public void CheckInventory()
+    {       
+        this.Inventory.CheckInventory();
+    }
+
+    public void PickObject(Items item)
+    {
+        this.Inventory.PickObject(item);
+    }
+
+    public void DropObject(Items item)
+    {
+        this.Inventory.DropObject(item);
+    }
+
+   
+}
