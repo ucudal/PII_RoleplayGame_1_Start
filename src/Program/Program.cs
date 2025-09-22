1763be92eacf00c922d07534222bd38eb75cece6
NahuelViq <nahuel.vique@correo.ucu.edu.uy>
2025-09-22 17:30:32 -0300

    Items
    Se agregaron dos elementos de armadura

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,5 +5,6 @@ public class Program
     //Creacion de items
     Items dragonSword = new Items("Dragon Sword", 0, 100, 0, "Arma");
     Items thanosSword = new Items("Thanos Sword", 0, 100, 0, "Arma");
-    
+    Items harlequinCrest = new Items("Harlequin Crest", 0, 0, 25, "Casco");
+    Items ScoundrelsLeathers = new Items("Scoundrel's Leathers", 0, 0, 30, "Pechera");
 }
