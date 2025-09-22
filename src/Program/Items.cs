3833fe5dcfcc63dd2b3f7aef9a9eba5f0dca9e67
Ignacio Alonso Sclavo <128947588+IgnacioAlonsoSclavo@users.noreply.github.com>
2025-09-22 16:16:18 -0300

    se_agrego_comentarios_y_constructor_
    Se agregaron los comentarios y el constructor de Items

--- src/Program/Items.cs
+++ src/Program/Items.cs
@@ -1,11 +1,21 @@
 namespace Program;
 
-public class Items
+public class Items //Creo clase Items
 {
+    //Atributos:
     public string Name { get; set; }
     public int MagicDamage { get; set; }
     public int Damage { get; set; }
     public int Armor { get; set; }
     public string Type { get; set; }
-    
+
+    //Constructor:
+    public Items(string name, int magicDamage, int damage, int armor, string type)
+    {
+        Name = name;
+        MagicDamage = magicDamage;
+        Damage = damage;
+        Armor = armor;
+        Type = type;
+    }
 }
